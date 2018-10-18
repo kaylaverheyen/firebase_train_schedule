0617a3f89e3d517e06bf4d01b5486f552d701efe
kaylaverheyen <kaylaverheyen@icloud.com>
2018-10-18 12:59:19 -0500

    momentjs added

--- javascript.js
+++ javascript.js
@@ -1,20 +1,8 @@
 var database = firebase.database();
 
-database.ref().on("child_added", function (snapshot) {
-    console.log(snapshot.val());
-    $("#train-table tbody").append(
-        `
-            <tr>
-                <td>${snapshot.val().train}</td>
-                <td>${snapshot.val().destination}</td>
-                <td>${snapshot.val().firstTime}</td>
-                <td></td>
-                <td>${snapshot.val().frequency}</td>
-                <td></td>
-            </tr>
-        `
-    );
-});
+// database.ref().on("child_added", function (snapshot) {
+//     console.log(snapshot.val());
+// });
 
 $("button").on("click", function (event) {
     event.preventDefault();
@@ -31,4 +19,49 @@ $("button").on("click", function (event) {
         "frequency": frequency
     });
 
+    //moment JS
+    var tFrequency = frequency;
+
+    //First Train Time
+    var start = firstTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(start, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    var arrivalTime = moment(nextTrain).format("hh:mm");
+    //append data:
+    $("#train-table tbody").append(
+        `
+    <tr>
+        <td>${train}</td>
+        <td>${destination}</td>
+        <td>${frequency}</td>
+        <td>${tMinutesTillTrain}</td>
+        <td>${arrivalTime}</td>
+    </tr>
+    `
+    );
+
 });
+
